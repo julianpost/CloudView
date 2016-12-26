071a7d3257c92dce42c3c01dc3dc33b552b43410
julianpost <julianedwardspost@gmail.com>
2016-12-26 13:53:34 -0500

    added a 2-thumb slider to the temperature view

--- CloudView/TempViewController.swift
+++ CloudView/TempViewController.swift
@@ -19,8 +19,9 @@ class TempViewController: UIViewController {
     @IBOutlet weak var minTempOneLbl: UILabel!
     @IBOutlet weak var maxTempOneLbl: UILabel!
     
+    @IBOutlet var slider2:UIXRangeSlider!
     
-    @IBAction func minTempOneChanged() {
+   /* @IBAction func minTempOneChanged() {
         mainSettingsData.minTempOne = Int(minTempOne.value)
         minTempOneLbl.text = "\(mainSettingsData.minTempOne)"
     }
@@ -28,6 +29,15 @@ class TempViewController: UIViewController {
     @IBAction func maxTempOneChanged() {
         mainSettingsData.maxTempOne = Int(maxTempOne.value)
         maxTempOneLbl.text = "\(mainSettingsData.maxTempOne)"
+        
+    }*/
+    
+    @IBAction func sliderChanged() {
+        
+        mainSettingsData.minTempOne = Int(slider2.leftValue)
+        mainSettingsData.maxTempOne = Int(slider2.rightValue)
+        minTempOneLbl.text = "\(mainSettingsData.minTempOne)"
+          maxTempOneLbl.text = "\(mainSettingsData.maxTempOne)"
         
     }
     
@@ -45,11 +55,38 @@ class TempViewController: UIViewController {
             UpdateView.handleTempCompletion(self.tempViewOne, temp: result)
         }
         
-        minTempOne.value = Float(mainSettingsData.minTempOne)
+        /*minTempOne.value = Float(mainSettingsData.minTempOne)
         maxTempOne.value = Float(mainSettingsData.maxTempOne)
         
         minTempOneLbl.text = "\(mainSettingsData.minTempOne)"
+        maxTempOneLbl.text = "\(mainSettingsData.maxTempOne)"*/
+        
+        slider2.leftValue = Float(mainSettingsData.minTempOne)
+        slider2.rightValue = Float(mainSettingsData.maxTempOne)
+        
+        minTempOneLbl.text = "\(mainSettingsData.minTempOne)"
         maxTempOneLbl.text = "\(mainSettingsData.maxTempOne)"
+        
+        //var image = UIImage(named: "roundedLeftThumb")!.withRenderingMode(.alwaysTemplate)
+        //self.slider2?.setImage(image, forElement: .leftThumb, forControlState: UIControlState())
+        self.slider2?.setTint(UIColor.red, forElement: .leftThumb, forControlState: UIControlState())
+        self.slider2?.setTint(UIColor.gray, forElement: .leftThumb, forControlState: .disabled)
+        
+        //image = UIImage(named: "roundedRightThumb")!.withRenderingMode(.alwaysTemplate)
+        //self.slider2?.setImage(image, forElement: .rightThumb, forControlState: UIControlState())
+        self.slider2?.setTint(UIColor.red, forElement: .rightThumb, forControlState: UIControlState())
+        self.slider2?.setTint(UIColor.gray, forElement: .rightThumb, forControlState: .disabled)
+        
+        self.slider2?.setTint(UIColor.red, forElement: .activeBar, forControlState: UIControlState())
+        self.slider2?.setTint(UIColor.gray, forElement: .activeBar, forControlState: .disabled)
+        
+        //self.slider2?.setTint(UIColor.clear, forElement: .middleThumb, forControlState: UIControlState())
+        
+        self.slider2?.setTint(UIColor.lightGray, forElement: .inactiveBar, forControlState: .disabled)
+        
+        //self.slider2!.barHeight = 2.0
+
+        
     }
     
     override func didReceiveMemoryWarning() {
